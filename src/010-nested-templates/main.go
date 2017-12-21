d86ab270f2f70f25a0e31a7287ba4f92b5e47463
Germán Valencia Vargas <gvalenncia@gmail.com>
2017-12-21 10:25:26 -0500

    Composing templates and passing data through them

--- src/010-nested-templates/main.go
+++ src/010-nested-templates/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"html/template"
+	"os"
+	"log"
+)
+
+var tpl *template.Template
+
+func init()  {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main()  {
+	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", "Germán")
+	if err != nil {
+		log.Fatalln("There was a problem when executing idex.gohtml", err)
+	}
+}
