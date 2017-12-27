ba46163d1abdfe90c6e046d1fe8ef5b96b959aa4
Germán Valencia Vargas <gvalenncia@gmail.com>
2017-12-26 19:02:29 -0500

    Doind all hands on excercises

--- src/022-hands-on-reloaded/main.go
+++ src/022-hands-on-reloaded/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"net"
 	"log"
-	"io"
+	"bufio"
+	"fmt"
+	"strings"
 )
 
 func main()  {
@@ -19,9 +21,32 @@ func main()  {
 		if err != nil {
 			log.Fatalln("There was a problem accepting connections", err)
 		}
-
-		io.WriteString(conn, "I see you connected")
-
-		conn.Close()
+		 go serve(conn)
 	}
+}
+
+func serve (conn net.Conn)  {
+	defer conn.Close()
+
+	sc := bufio.NewScanner(conn)
+	for i :=0 ; sc.Scan(); i++ {
+		ln := sc.Text()
+
+		if i == 0 {
+			fields := strings.Fields(ln)
+			fmt.Print(fields[0])
+			fmt.Print(fields[1])
+		}
+
+		if ln == "" {
+			break
+		}
+	}
+
+	body := "<h1>HOLY COW THIS IS LOW LEVEL</h1>"
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
 }
